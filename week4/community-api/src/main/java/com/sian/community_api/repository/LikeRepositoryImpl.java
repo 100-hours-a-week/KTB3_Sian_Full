b2a682d6e5285dda704ee7e65bed115e2f507b46
Dindory <22jysong@gmail.com>
2025-10-21 17:13:45 +0900

    Update LikeRepositoryImpl.java

--- week4/community-api/src/main/java/com/sian/community_api/repository/LikeRepositoryImpl.java
+++ week4/community-api/src/main/java/com/sian/community_api/repository/LikeRepositoryImpl.java
@@ -11,16 +11,19 @@ import java.util.List;
 public class LikeRepositoryImpl implements LikeRepository {
     private static final List<Like> store = new ArrayList<>();
 
+    @Override
     public boolean hasUserLiked(Long postId, Long userId) {
         return store.stream()
                 .anyMatch(like -> like.getPostId().equals(postId)
                         && like.getUserId().equals(userId));
     }
 
+    @Override
     public void save(Like like) {
         store.add(like);
     }
 
+    @Override
     public void delete(Long postId, Long userId) {
         store.removeIf(like -> like.getPostId().equals(postId)
                 && like.getUserId().equals(userId));

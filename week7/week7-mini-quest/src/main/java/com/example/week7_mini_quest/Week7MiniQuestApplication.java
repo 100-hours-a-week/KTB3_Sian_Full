281883da2c1e8afdb687c29d8101d81edbdc4163
Dindory <22jysong@gmail.com>
2025-10-28 15:08:19 +0900

    chore: 미니퀘스트 H2 DB 연결

---
+++ week7/week7-mini-quest/src/main/java/com/example/week7_mini_quest/Week7MiniQuestApplication.java
@@ -0,0 +1,13 @@
+package com.example.week7_mini_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7MiniQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week7MiniQuestApplication.class, args);
+	}
+
+}
---
+++ week7/week7-mini-quest/src/test/java/com/example/week7_mini_quest/Week7MiniQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week7_mini_quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7MiniQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
